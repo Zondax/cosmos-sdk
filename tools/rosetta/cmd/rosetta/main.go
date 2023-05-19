88f1a4b8247064544df7f58a7895eaa3fd96bf67
bizk <santiago.yanzon1999@gmail.com>
2023-05-19 17:05:47 -0300

    Added lightclients tendermint interface

--- tools/rosetta/cmd/rosetta/main.go
+++ tools/rosetta/cmd/rosetta/main.go
@@ -1,19 +1,22 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
 	"cosmossdk.io/log"
 	rosettaCmd "cosmossdk.io/tools/rosetta/cmd"
 	"github.com/cosmos/cosmos-sdk/codec"
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	"github.com/cosmos/cosmos-sdk/x/auth/tx"
 
 	ibcclienttypes "github.com/cosmos/ibc-go/v7/modules/core/02-client/types"
-	ibcLightClient "github.com/cosmos/ibc-go/v7/modules/light-clients/07-tendermint"
 	"github.com/cosmos/ibc-go/v7/modules/core/exported"
+	ibcLightClient "github.com/cosmos/ibc-go/v7/modules/light-clients/07-tendermint"
+
 	//ibcLightClientTypes "github.com/cosmos/ibc-go/v7/modules/core/exported"
 	//sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/std"
+	testCodec "github.com/cosmos/cosmos-sdk/types/module/testutil"
 )
 
 func main() {
@@ -26,7 +29,15 @@ func main() {
 	ibcclienttypes.RegisterInterfaces(interfaceRegistry)
 	ibcLightClient.RegisterInterfaces(interfaceRegistry)
 	interfaceRegistry.RegisterInterface("/ibc.lightclients.tendermint.v1.Header", (*exported.ClientMessage)(nil))
-	fmt.Println(interfaceRegistry.ListAllInterfaces())
+	aminoCodec := codec.NewLegacyAmino()
+	encCfg := testCodec.TestEncodingConfig{
+		InterfaceRegistry: interfaceRegistry,
+		Codec:             cdc,
+		TxConfig:          tx.NewTxConfig(cdc, tx.DefaultSignModes),
+		Amino:             aminoCodec,
+	}
+	std.RegisterLegacyAminoCodec(encCfg.Amino)
+	std.RegisterInterfaces(encCfg.InterfaceRegistry)
 
 	if err := rosettaCmd.RosettaCommand(interfaceRegistry, cdc).Execute(); err != nil {
 		logger.Error("failed to run rosetta", "error", err)
