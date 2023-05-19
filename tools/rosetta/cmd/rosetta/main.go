19d56994766d0f993bcd5074d532d7a051fc4043
bizk <santiago.yanzon1999@gmail.com>
2023-05-19 13:03:08 -0300

    Added missing ibc interfaces

--- tools/rosetta/cmd/rosetta/main.go
+++ tools/rosetta/cmd/rosetta/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"cosmossdk.io/log"
 	rosettaCmd "cosmossdk.io/tools/rosetta/cmd"
 	"github.com/cosmos/cosmos-sdk/codec"
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+
+	ibcclienttypes "github.com/cosmos/ibc-go/v7/modules/core/02-client/types"
+	//sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 func main() {
@@ -15,6 +19,8 @@ func main() {
 		interfaceRegistry = codectypes.NewInterfaceRegistry()
 		cdc               = codec.NewProtoCodec(interfaceRegistry)
 	)
+
+	ibcclienttypes.RegisterInterfaces(interfaceRegistry)
 
 	if err := rosettaCmd.RosettaCommand(interfaceRegistry, cdc).Execute(); err != nil {
 		logger.Error("failed to run rosetta", "error", err)
