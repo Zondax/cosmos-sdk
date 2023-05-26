24420d917fa63b4703e762b0e22b945d4c9a5b22
rllola <rllola80@gmail.com>
2023-05-26 18:06:51 +0200

    added the missing interfaces for osmosis

--- tools/rosetta/plugins/osmosis/main.go
+++ tools/rosetta/plugins/osmosis/main.go
@@ -7,7 +7,11 @@ import (
 	cryptocodec "github.com/cosmos/cosmos-sdk/crypto/codec"
 	ibcclienttypes "github.com/cosmos/ibc-go/v7/modules/core/02-client/types"
 
+	//	"github.com/cosmos/ibc-go/v7/modules/core/exported"
+	ibcChannelClient "github.com/cosmos/ibc-go/v7/modules/core/04-channel/types"
 	ibcLightClient "github.com/cosmos/ibc-go/v7/modules/light-clients/07-tendermint"
+
+	cosmosWasmClient "github.com/CosmWasm/wasmd/x/wasm"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	txtypes "github.com/cosmos/cosmos-sdk/types/tx"
@@ -30,6 +34,8 @@ func RegisterInterfaces(registry codectypes.InterfaceRegistry) {
 
 	ibcclienttypes.RegisterInterfaces(registry)
 	ibcLightClient.RegisterInterfaces(registry)
+	ibcChannelClient.RegisterInterfaces(registry)
+	cosmosWasmClient.RegisterInterfaces(registry)
 	sdk.RegisterInterfaces(registry)
 	txtypes.RegisterInterfaces(registry)
 	cryptocodec.RegisterInterfaces(registry)
